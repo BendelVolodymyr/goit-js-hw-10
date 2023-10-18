6bd66d9ad71982dda63955ea7c1fe824831d2dec
BendelVolodymyr <bendel.vova@gmail.com>
2023-10-18 23:04:29 +0200

    two

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
-
-
 import { fetchBreeds } from './js/cat-api';
+import { fetchCatByBreed } from './js/cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
-const test = fetchBreeds().then();
-console.log(fetchBreeds());
+
+// console.log(fetchCatByBreed('abys'))
+
+const selectEl = document.querySelector('.breed-select');
+const pLoaderEl = document.querySelector('.loader');
+const pErrorEl = document.querySelector('.error');
+const divCatInfoEl = document.querySelector('.cat-info');
+
+selectEl.addEventListener('change', test)
+
+function test() {
+    const target = this.value;
+    return fetchCatByBreed(target).then((data) => {
+        data.forEach(({ url, height, width}) => {
+            const newDivEl = document.createElement('div');
+            newDivEl.style.cssText = `
+            background-image: url("${url}");
+            height: ${height}px;
+            width: ${width}px;`;
+            return divCatInfoEl.append(newDivEl);
+        })
+        
+    })
+}
+// 
+fetchBreeds().then((data) => {
+    let allCats = []; 
+    new SlimSelect({
+    select: '.breed-select',
+    settings: {
+    placeholderText: 'Choose a cat',
+  }
+});
+    data.forEach(({name, id}) => {
+        const newOptionEl = document.createElement('option');
+        newOptionEl.textContent = name;
+        newOptionEl.value = id;
+        allCats.push(newOptionEl);
+    });;
+    return selectEl.append(...allCats);
+});
+
+
+
+
+// console.log(test);
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,17 +1,22 @@
 export const fetchBreeds = () => {
-    const BASE_URL = 'https://api.thecatapi.com/v1';
+    const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
     const API_KEY = 'live_yhKx5EW7r5s5EKWButootd2z8zjtwxwBhL2KtG8Vaujfjusynd9eugh1xm5dCi7E';
 
-return fetch(`${BASE_URL}/breeds?${API_KEY}`).then(response => {
+return fetch(`${BASE_URL}?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
         throw new Error(response.statusText || 'Помилка');
     }
     return response.json();
-}).then(data => {
-    return data;
-})
-};
+})};
 
 export const fetchCatByBreed = (breedId) => {
+    const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+    const API_KEY = 'live_yhKx5EW7r5s5EKWButootd2z8zjtwxwBhL2KtG8Vaujfjusynd9eugh1xm5dCi7E';
 
+    return fetch(`${BASE_URL}?breed_ids=${breedId}&${API_KEY}`).then((response) => {
+        if (!response.ok) {
+        throw new Error(response.statusText || 'Помилка');
+    }
+    return response.json();
+    })
 };
