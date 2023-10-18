395602f997b04a5679f5db1f4497caeac3777ed2
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-10-18 21:04:56 +0000

    Deploying to gh-pages from @ BendelVolodymyr/goit-js-hw-10@6bd66d9ad71982dda63955ea7c1fe824831d2dec 🚀

--- index.76a9e773.js
+++
@@ -1,2 +0,0 @@
-const t=()=>fetch("https://api.thecatapi.com/v1/breeds?live_yhKx5EW7r5s5EKWButootd2z8zjtwxwBhL2KtG8Vaujfjusynd9eugh1xm5dCi7E").then((t=>{if(!t.ok)throw new Error(t.statusText||"Помилка");return t.json()})).then((t=>t));t().then();console.log(t());
-//# sourceMappingURL=index.76a9e773.js.map
